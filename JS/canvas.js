47038e5cb8f478f76b590069588008b37fb016c0
Kushang Shah <kushangshah131@gmail.com>
2020-07-30 07:40:51 +0530

    Update canvas.js

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,1 +1,24 @@
 let DrawArea = document.getElementById('drawCanvas');
+let ctx = DrawArea.getContext('2d');
+
+class Draw{
+	
+	ctx;
+	constructor(ctx){
+		this.ctx = ctx;
+	}
+	
+	rectangle(x, y, height, width, otcolor = "white", bgcolor = "black"){
+		this.ctx.beginPath();
+		this.ctx.rect(x, y, height, width);
+		ctx.fillStyle = otcolor;
+		this.ctx.fill();
+		ctx.fillStyle = bgcolor;
+		this.ctx.stroke();
+		this.ctx.closePath();
+	}
+	
+}
+
+let draw = new Draw(ctx);
+draw.rectangle();
