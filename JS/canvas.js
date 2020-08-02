18d2366f4ae7696365ff7fff80c4290eccbcf710
Kushang Shah <kushangshah131@gmail.com>
2020-08-02 15:36:03 +0530

    Update canvas.js

--- JS/canvas.js
+++ JS/canvas.js
@@ -30,12 +30,24 @@ class Draw{
 		this.ctx.closePath();
 	}
 	
+	circle(x, y, r, sAngle, eAngle, otcolor = "white", bgcolor = "black", direction = false){
+		
+		this.ctx.beginPath();
+		this.ctx.arc(x, y, r, sAngle, eAngle, direction);
+		this.ctx.fillStyle = bgcolor;
+		this.ctx.fill();
+		ctx.strokeStyle = otcolor;
+		this.ctx.stroke();
+		this.ctx.closePath();
+		
+	}
+	
 }
 
 
 //variables and objects
 let draw = new Draw(ctx);
-let x = 0;
+let x = 30;
 let drawable = 0; //can be 0 or 1
 // 0 for not drawable and 1 for drawable
 
@@ -49,6 +61,7 @@ DrawArea.addEventListener('click',()=>{
 //continuous iterations
 setInterval(()=>{
 	
-
+	draw.circle(x, 30, 20, 0, Math.PI*2);
 	
-}, 100);
+	
+}, 1);
