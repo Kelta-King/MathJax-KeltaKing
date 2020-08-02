2a247212e7bc0d8709f67bbd5e9f43b7bdf6aee7
Kushang Shah <kushangshah131@gmail.com>
2020-08-02 15:25:27 +0530

    Update canvas.js

--- JS/canvas.js
+++ JS/canvas.js
@@ -1,6 +1,18 @@
 let DrawArea = document.getElementById('drawCanvas');
 let ctx = DrawArea.getContext('2d');
 
+//functions
+let curx = 0;
+let cury = 0;
+
+function coordinate(event) { 
+  
+    curx = event.clientX;
+    cury = event.clientY;
+	
+} 
+
+//drawign classes
 class Draw{
 	
 	ctx;
@@ -8,17 +20,35 @@ class Draw{
 		this.ctx = ctx;
 	}
 	
-	rectangle(x, y, height, width, otcolor = "white", bgcolor = "black"){
+	rectangle(x, y, width, height, otcolor = "white", bgcolor = "black"){
 		this.ctx.beginPath();
-		this.ctx.rect(x, y, height, width);
-		ctx.fillStyle = otcolor;
-		this.ctx.fill();
+		this.ctx.rect(x, y, width, height);
 		ctx.fillStyle = bgcolor;
+		this.ctx.fill();
+		ctx.strokeStyle = otcolor;
 		this.ctx.stroke();
 		this.ctx.closePath();
 	}
 	
 }
 
+
+//variables and objects
 let draw = new Draw(ctx);
-draw.rectangle();
+let x = 0;
+let drawable = 0; //can be 0 or 1
+// 0 for not drawable and 1 for drawable
+
+
+//Event listners
+DrawArea.addEventListener('click',()=>{
+	drawable = 1;
+});
+
+
+//continuous iterations
+setInterval(()=>{
+	
+
+	
+}, 100);
